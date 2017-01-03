7be89de845cd86855e1d0c867fcb3ef51f5030a7
ariegg <brain@t-h-i-n-x.net>
2017-01-03 11:16:14 +0100

    Create drivers.py

---
+++ chips/sensor/honxxxpressure/drivers.py
@@ -0,0 +1,4 @@
+#   This code has to be added to the corresponding __init__.py
+
+DRIVERS["honXXXpressure"] = ["HONXSCPI", "HONXSCPTI", "HONXSCPS", "HONXSCPTS", "HONABPPI", "HONABPPTI", "HONABPPS", "HONABPPTS"]
+
