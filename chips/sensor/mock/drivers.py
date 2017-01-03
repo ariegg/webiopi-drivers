12dcb0fe8ae8ab51b1ab66dcc8bc507d029b0518
ariegg <brain@t-h-i-n-x.net>
2017-01-03 11:17:01 +0100

    Create drivers.py

---
+++ chips/sensor/mock/drivers.py
@@ -0,0 +1,4 @@
+#   This code has to be added to the corresponding __init__.py
+
+DRIVERS["sensormock"] = ["PRESSURE", "TEMPERATURE", "LUMINOSITY", "DISTANCE", "HUMIDITY", "COLOR", "CURRENT", "VOLTAGE", "POWER", "SENSORS"]
+
