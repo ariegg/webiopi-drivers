3db492fd221e9ddf1fd178007ef4c7208530fe6a
ariegg <brain@t-h-i-n-x.net>
2017-01-03 10:33:36 +0100

    Create current.py

---
+++ abstractions/sensor/current/current.py
@@ -0,0 +1,28 @@
+class Current():
+    def __family__(self):
+        return "Current"
+
+    def __getMilliampere__(self):
+        raise NotImplementedError
+
+    @api("Current", 0)
+    @request("GET", "sensor/current/*")
+    @response(contentType=M_JSON)
+    def currentWildcard(self):
+        values = {}
+        current = self.__getMilliampere__()
+        values["mA"] = "%.03f" % current
+        values["A"]  = "%.03f" % (current * 1000)
+        return values
+
+    @api("Current")
+    @request("GET", "sensor/current/mA")
+    @response("%.03f")
+    def getMilliampere(self):
+        return self.__getMilliampere__()
+
+    @api("Current")
+    @request("GET", "sensor/current/A")
+    @response("%.03f")
+    def getAmpere(self):
+        return self.__getMilliampere__() * 1000
