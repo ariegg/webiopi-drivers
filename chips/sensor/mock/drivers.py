20597808c2947f9768fe49699e8ab008cfed982a
ariegg <brain@t-h-i-n-x.net>
2017-01-13 17:04:41 +0100

    Update drivers.py

--- chips/sensor/mock/drivers.py
+++ chips/sensor/mock/drivers.py
@@ -1,4 +1,6 @@
 #   This code has to be added to the corresponding __init__.py
 
-DRIVERS["sensormock"] = ["PRESSURE", "TEMPERATURE", "LUMINOSITY", "DISTANCE", "HUMIDITY", "COLOR", "CURRENT", "VOLTAGE", "POWER", "SENSORS"]
-
+DRIVERS["sensormock"] = ["PRESSURE", "TEMPERATURE", "LUMINOSITY", "DISTANCE", "HUMIDITY",
+                         "COLOR", "CURRENT", "VOLTAGE", "POWER",
+                         "LINEARACCELERATION", "ANGULARACCELERATION", "ACCELERATION", "LINEARVELOCITY", "ANGULARVELOCITY", "VELOCITY",
+                         "SENSORS"]
