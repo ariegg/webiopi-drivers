8fb9b1255e039b2b72bc624afc18c948b0782f55
ariegg <brain@t-h-i-n-x.net>
2017-01-13 16:56:22 +0100

    Update power.monitor.js

--- abstractions/sensor/power/power.monitor.js
+++ abstractions/sensor/power/power.monitor.js
@@ -4,11 +4,47 @@ WebIOPi.prototype.newDevice = function(type, name) {
 
 // ...
 
+	if (type == "Power") {
+		return new Power(name);
+	}
 
-
-	return undefined;
+// ...
+	
 }
 
 // ...
 
 // ----- Power -----
+
+function Power(name) {
+	this.name = name;
+	this.url = "/devices/" + name + "/sensor";
+	this.refreshTime = 1000;
+}
+
+Power.prototype.toString = function() {
+	return this.name + ": Power";
+}
+
+
+Power.prototype.getWatt = function(callback) {
+	$.get(this.url + "/power/W", function(data) {
+		callback(this.name, data);
+	});
+}
+
+Power.prototype.refreshUI = function() {
+	var temp = this;
+	var element = this.element;
+	if ((element != undefined) && (element.header == undefined)) {
+		element.header = $("<h3>" + this + "</h3>");
+		element.append(element.header);
+	}
+	
+	this.getWatt(function(name, data){
+		if (element != undefined) {
+			element.header.text(temp + ": " + data + " W");
+		}
+		setTimeout(function(){temp.refreshUI()}, temp.refreshTime);
+	});
+}
