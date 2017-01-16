cb375d52a91042508b51099f051193d4f10e71d6
ariegg <brain@t-h-i-n-x.net>
2017-01-16 18:22:31 +0100

    Delete drivers.py

--- chips/sensor/mock/drivers.py
+++
@@ -1,6 +0,0 @@
-#   This code has to be added to the corresponding __init__.py
-
-DRIVERS["sensormock"] = ["PRESSURE", "TEMPERATURE", "LUMINOSITY", "DISTANCE", "HUMIDITY",
-                         "COLOR", "CURRENT", "VOLTAGE", "POWER",
-                         "LINEARACCELERATION", "ANGULARACCELERATION", "ACCELERATION", "LINEARVELOCITY", "ANGULARVELOCITY", "VELOCITY",
-                         "SENSORS"]
