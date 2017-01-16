74a4f56d28497de89415f29ca3e1d6298c2fdd23
ariegg <brain@t-h-i-n-x.net>
2017-01-16 18:22:03 +0100

    Create drivers.py

---
+++ chips/sensor/simulation/drivers.py
@@ -0,0 +1,6 @@
+#   This code has to be added to the corresponding __init__.py
+
+DRIVERS["simulatedsensors"] = ["PRESSURE", "TEMPERATURE", "LUMINOSITY", "DISTANCE", "HUMIDITY",
+                         "COLOR", "CURRENT", "VOLTAGE", "POWER",
+                         "LINEARACCELERATION", "ANGULARACCELERATION", "ACCELERATION", "LINEARVELOCITY", "ANGULARVELOCITY", "VELOCITY",
+"SENSORS"]
